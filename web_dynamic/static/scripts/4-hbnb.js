bda1038280480ca8fe78e822748a94534979ea66
DiegoCol93 <dfelo93@gmail.com>
2021-05-15 23:01:20 -0500

    |- FIXED -| Semistandard in proper version.

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -3,6 +3,8 @@ const d = document;
 let amenityIdList = [];
 let amenityNamesList = [];
 const fetch = window.fetch;
+// const urlBase = 'http://0.0.0.0:5001/api/v1';
+const urlBase = 'http://127.0.0.1:5001/api/v1';
 
 // Status response api fetch. - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
 async function statusResponse (url) {
@@ -16,6 +18,74 @@ async function statusResponse (url) {
     $('#api_status').addClass('unavailable');
   }
 }
+
+// Populates the place elements - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+function populatePlaces (data) {
+  data.forEach(place => {
+    // create Article tag
+    const articleNode = d.createElement('ARTICLE');
+    // Create title box div
+    let outterDiv = d.createElement('DIV');
+    const placeNameH2 = d.createElement('H2');
+    let innerDiv = d.createElement('DIV');
+    placeNameH2.appendChild(d.createTextNode(place.name));
+    innerDiv.appendChild(d.createTextNode('$' + place.price_by_night));
+    outterDiv.appendChild(placeNameH2);
+    outterDiv.appendChild(innerDiv);
+    articleNode.appendChild(outterDiv);
+    outterDiv.classList.add('title_box');
+    innerDiv.classList.add('price_by_night');
+    // end of first div
+
+    // Information div
+    outterDiv = d.createElement('DIV');
+    innerDiv = d.createElement('DIV');
+    innerDiv.classList.add('max_guest');
+    const guest = place.max_guest !== 1 ? 'Guests' : 'Guest';
+    innerDiv.appendChild(d.createTextNode(guest + ' ' + place.max_guest));
+    outterDiv.appendChild(innerDiv);
+
+    innerDiv = d.createElement('DIV');
+    innerDiv.classList.add('number_rooms');
+    const room = place.max_guest !== 1 ? 'Rooms' : 'Room';
+    innerDiv.appendChild(d.createTextNode(room + ' ' + place.number_rooms));
+    outterDiv.appendChild(innerDiv);
+
+    innerDiv = d.createElement('DIV');
+    innerDiv.classList.add('number_bathrooms');
+    const bathroom = place.max_guest !== 1 ? 'Bathrooms' : 'Bathroom';
+    innerDiv.appendChild(d.createTextNode(bathroom + ' ' + place.number_bathrooms));
+    outterDiv.appendChild(innerDiv);
+
+    outterDiv.classList.add('information');
+    articleNode.appendChild(outterDiv);
+
+    // Description div
+    outterDiv = d.createElement('DIV');
+    outterDiv.classList.add('description');
+    outterDiv.insertAdjacentHTML('beforeend', place.description);
+    articleNode.appendChild(outterDiv);
+
+    $('.places')[0].appendChild(articleNode);
+  });
+}
+
+// Request API's place data. - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+function getPlaceData (url, filters, successCallBack) {
+  $.ajax({
+    url: urlBase + '/places_search',
+    crossDomain: true,
+    type: 'post',
+    dataType: 'json',
+    contentType: 'application/json',
+    data: JSON.stringify(filters),
+    success: successCallBack,
+    error: function () {
+      console.log('Cannot get data');
+    }
+  });
+}
+
 // Wrapper for waiting page to complete loading. - - - - - - - - - - - - - - - - - - - |
 document.onreadystatechange = function () {
   if (document.readyState === 'complete') {
@@ -44,81 +114,12 @@ document.onreadystatechange = function () {
       $('.amenities h4').text(amenityNamesList.join(', '));
     });
 
-    // const urlBase = 'http://0.0.0.0:5001/api/v1';
-    const urlBase = 'http://127.0.0.1:5001/api/v1';
-
     // Status light display call - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
     statusResponse(urlBase + '/status');
     // setInterval(statusResponse, 10000, url);
 
-    // Request API's place data. - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
-    function getPlaceData (url, filters, successCallBack) {
-      $.ajax({
-        url: urlBase + '/places_search',
-        crossDomain: true,
-        type: 'post',
-        dataType: 'json',
-        contentType: 'application/json',
-        data: JSON.stringify(filters),
-        success: successCallBack,
-        error: function () {
-          console.log('Cannot get data');
-        }
-      });
-    }
-
+    // Call the getplacedata and populateplaces functions on first load once - - - - - - |
     getPlaceData('', {}, populatePlaces);
-
-    // Populate the place elements - - - - - - - - - - - - - - - - - - - - - - - - - - - |
-    function populatePlaces (data) {
-      data.forEach(place => {
-        // create Article tag
-        const articleNode = d.createElement('ARTICLE');
-        // Create title box div
-        let outterDiv = d.createElement('DIV');
-        const placeNameH2 = d.createElement('H2');
-        let innerDiv = d.createElement('DIV');
-        placeNameH2.appendChild(d.createTextNode(place.name));
-        innerDiv.appendChild(d.createTextNode('$' + place.price_by_night));
-        outterDiv.appendChild(placeNameH2);
-        outterDiv.appendChild(innerDiv);
-        articleNode.appendChild(outterDiv);
-        outterDiv.classList.add('title_box');
-        innerDiv.classList.add('price_by_night');
-        // end of first div
-
-        // Information div
-        outterDiv = d.createElement('DIV');
-        innerDiv = d.createElement('DIV');
-        innerDiv.classList.add('max_guest');
-        const guest = place.max_guest !== 1 ? 'Guests' : 'Guest';
-        innerDiv.appendChild(d.createTextNode(guest + ' ' + place.max_guest));
-        outterDiv.appendChild(innerDiv);
-
-        innerDiv = d.createElement('DIV');
-        innerDiv.classList.add('number_rooms');
-        const room = place.max_guest !== 1 ? 'Rooms' : 'Room';
-        innerDiv.appendChild(d.createTextNode(room + ' ' + place.number_rooms));
-        outterDiv.appendChild(innerDiv);
-
-        innerDiv = d.createElement('DIV');
-        innerDiv.classList.add('number_bathrooms');
-        const bathroom = place.max_guest !== 1 ? 'Bathrooms' : 'Bathroom';
-        innerDiv.appendChild(d.createTextNode(bathroom + ' ' + place.number_bathrooms));
-        outterDiv.appendChild(innerDiv);
-
-        outterDiv.classList.add('information');
-        articleNode.appendChild(outterDiv);
-
-        // Description div
-        outterDiv = d.createElement('DIV');
-        outterDiv.classList.add('description');
-        outterDiv.insertAdjacentHTML('beforeend', place.description);
-        articleNode.appendChild(outterDiv);
-
-        $('.places')[0].appendChild(articleNode);
-      });
-    }
 
     // On click event on search button action. - - - - - - - - - - - - - - - - - - - - - |
     $('button').on('click', function () {
